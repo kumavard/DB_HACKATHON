a9e3324e69b9183bfefcd11ed6a4ac1fdecd4fd0
Varun Nair <42412474+kumavard@users.noreply.github.com>
2019-10-10 14:40:38 +0530

    Add files via upload

---
+++ CO2FootprintApplicationTest.java
@@ -0,0 +1,16 @@
+package com.co2.footprint;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CO2FootprintApplicationTest {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
